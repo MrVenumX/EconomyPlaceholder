96b1adfd03043e778a09e4242d20e2faea64b361
mrvenumx <killthegame180408@gmail.com>
2024-10-30 16:44:53 +0700

    First commit

---
+++ src/main/kotlin/com/mrvenumx/nukkit/plugins/EconomyPlaceholder.kt
@@ -0,0 +1,28 @@
+package com.mrvenumx.nukkit.plugins
+
+import java.text.NumberFormat
+
+import cn.nukkit.plugin.PluginBase
+import com.creeperface.nukkit.placeholderapi.api.PlaceholderAPI
+import me.onebone.economyapi.EconomyAPI
+
+class EconomyPlaceholder : PluginBase() {
+    override fun onEnable() {
+        super.onEnable()
+        val papi = PlaceholderAPI.getInstance()
+
+        with(server.pluginManager) {
+            getPlugin("EconomyAPI")?.let {
+                val eco = EconomyAPI.getInstance()
+
+                logger.debug("EconomyAPI is loaded!")
+                papi.build<String>("eco_money") {
+                    visitorLoader {
+                        NumberFormat.getInstance().format(eco.myMoney(player))
+                    }
+                }
+            } ?: logger.warning("plugin EconomyAPI was not found, please install it!")
+
+        }
+    }
+}
